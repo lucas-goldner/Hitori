147fe75853825dfa74bec566fe38965ddfc9b85e
lucas-goldner <lucas.goldner@googlemail.com>
2021-04-17 00:47:09 +0200

    feat: Basic Home Setup

--- app/src/main/java/com/lucasgoldner/hitori/MainActivity.kt
+++ app/src/main/java/com/lucasgoldner/hitori/MainActivity.kt
@@ -7,7 +7,6 @@ import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
