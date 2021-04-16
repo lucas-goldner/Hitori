05c8aaf82ecbcb1f8798bda765d8cb64efeb3448
lucas-goldner <lucas.goldner@googlemail.com>
2021-04-17 00:15:18 +0200

    feat: Customize app theme

--- app/src/main/java/com/lucasgoldner/hitori/MainActivity.kt
+++ app/src/main/java/com/lucasgoldner/hitori/MainActivity.kt
@@ -2,9 +2,12 @@ package com.lucasgoldner.hitori
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
