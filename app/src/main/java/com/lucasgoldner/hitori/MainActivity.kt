1bbe50fd60e8d02859b506043446553af149002e
lucas-goldner <lucas.goldner@googlemail.com>
2021-04-17 03:09:49 +0200

    feat: Add opening animation

--- app/src/main/java/com/lucasgoldner/hitori/MainActivity.kt
+++ app/src/main/java/com/lucasgoldner/hitori/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.Window
 import android.view.WindowManager
 import android.widget.Button
+import android.widget.LinearLayout
 import androidx.compose.Composable
 import com.github.florent37.kotlin.pleaseanimate.please
 
@@ -20,14 +21,19 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun setupView() {
+        var layout = findViewById<LinearLayout>(R.id.bottomBarLayout)
         var home = findViewById<Button>(R.id.homeButton)
         var lessons = findViewById<Button>(R.id.lessonsButton)
         var notes = findViewById<Button>(R.id.notesButton)
+        layout.layoutParams.height = 0
         please {
-            animate(home) toBe {
-                bottomOfItsParent(marginDp = 36f)
-                leftOfItsParent(marginDp = 16f)
-                width(40, keepRatio = true, toDp = true)
+            animate(layout) toBe {
+
+                height(150, keepRatio = true, toDp = true)
+                bottomOfItsParent(marginDp = 30f)
+                //bottomOfItsParent(marginDp = 36f)
+                //leftOfItsParent(marginDp = 16f)
+                //width(40, keepRatio = true, toDp = true)
             }
         }.start()
 
